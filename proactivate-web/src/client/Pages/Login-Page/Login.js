6903d6828691bfc39d09e13680570ca2ebf52adc
Serena Tzeng <stzeng2@illinois.edu>
2022-03-04 15:37:33 -0600

    added Google login functionality for the frontend (working correctly, but still need to work on the backend connection for Google login)

--- proactivate-web/src/client/Pages/Login-Page/Login.js
+++ proactivate-web/src/client/Pages/Login-Page/Login.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 import ModalDialog2 from '../../Components/ModalDialog2';
+import GoogleLogin from 'react-google-login';
+import axios from 'axios';
 
 
 const Login = () => {
@@ -14,11 +16,34 @@ const Login = () => {
     setOpen(false);
   };
 
+  /* for google login */
+  const responseSuccessGoogle = (response) => {
+    console.log(response);
+    axios({
+      method: 'POST',
+      url: 'http://localhost:5000/api/googlelogin',
+      data: {tokenId: response.tokenId}
+    }).then(response => {
+      console.log(response);
+    })
+  }
+
+  const responseErrorGoogle = (response) => {
+    console.log(response);
+  }
+
   return (
    <div className="Login">
      <Button variant="contained" color="primary" onClick={handleOpen}>
     Login
     </Button>
+    <GoogleLogin
+    clientId="393335656297-7iu82fqvv69527r36jk2f7c6d0s7i9sg.apps.googleusercontent.com"
+    buttonText="Login with Google"
+    onSuccess={responseSuccessGoogle}
+    onFailure={responseErrorGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
       <ModalDialog2 open={open} handleClose={handleClose} />
    </div>
     
