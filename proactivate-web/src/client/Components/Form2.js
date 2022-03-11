7368cc0654634c6d152d1324cfc69f07038361bc
Divya Sathiyamoorthy <sdivya2408@gmail.com>
2022-03-11 13:57:15 -0600

    Fixed UI for Login Page

--- proactivate-web/src/client/Components/Form2.js
+++ proactivate-web/src/client/Components/Form2.js
@@ -1,24 +1,27 @@
-
 import { TextField } from '@mui/material';
 import { makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import React, { useState } from 'react';
+import './Form.css';
+import './fonts.css'
+import { Link } from 'react-router-dom';
 
 
 const useStyles = makeStyles(theme => ({
     root: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: theme.spacing(2),
-  '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: '300px',
-      },
-  '& .MuiButtonBase-root': {
-        margin: theme.spacing(2),
-      },
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: theme.spacing(10),
+    '& .MuiTextField-root': {
+          margin: theme.spacing(2),
+          width: '300px',
+        },
+    '& .MuiButtonBase-root': {
+          margin: theme.spacing(2),
+          width: '300px',
+        },
     },
   }));
 
@@ -37,8 +40,9 @@ const Form2 = ({ handleClose }) => {
 
   return (
     <form className={classes.root}>
+        <h className = "title">Welcome Back</h>
         <TextField 
-            label="User Name" 
+            label="User Name"
             variant="outlined" 
             required
             value={userName}
@@ -52,14 +56,17 @@ const Form2 = ({ handleClose }) => {
             value={password}
             onChange={e => setPassword(e.target.value)}
         />
-
         <div>
-            <Button variant="contained">
-                Log in with Google
-            </Button>
-            <Button type="submit" variant="contained" color="primary">
+            <Button className = "button1" type="submit" variant="contained"   >
                 Login
             </Button>
+            <p className='paragraph'> ------------------------------ OR -------------------------------</p>
+            <p className='logRout'> 
+             <Link className='logRout_b' to="/Login"> Login with Google</Link>
+            </p>
+            <p className='logRout'> Don't have an account? 
+             <Link className='logRout_b' to="/SignUp"> Sign-Up</Link>
+            </p>
         </div>
     </form>
   );
