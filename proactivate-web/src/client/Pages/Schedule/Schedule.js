cccf7a6c3be047f0eba29e888138386ff29ac73a
Divya Sathiyamoorthy <sdivya2408@gmail.com>
2022-03-24 13:11:42 -0500

    todo list backbone

--- proactivate-web/src/client/Pages/Schedule/Schedule.js
+++ proactivate-web/src/client/Pages/Schedule/Schedule.js
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Schedule.css";
+import Form from "./Form";
+import TodoList from "./TodoList";
+
 const Schedule = () => {
-  return <h1>Im in Schedule</h1>;
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        getLocalTodos();
+    }, []);
+
+    useEffect(() => {
+        saveLocalTodos();
+    }, [todos]);
+
+    const saveLocalTodos = () => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    };
+
+    const getLocalTodos = () => {
+        if (localStorage.getItem("todos") === null) {
+            localStorage.setItem("todos", JSON.stringify([]));
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem("todos"));
+            setTodos(todoLocal);
+        }
+    };
+
+    return (
+      <div className="Schedule">
+          <header>
+              <h1>Your schedule</h1>
+          </header>
+          <Form 
+            todos = {todos} 
+            setTodos={setTodos} 
+            inputText= {inputText} 
+            setInputText={setInputText} 
+          />
+          <TodoList setTodos={setTodos} todos = {todos}/>
+      </div>
+    );
 };
 
 export default Schedule;
