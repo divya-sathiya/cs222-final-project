54e5548bc5a7566a775cdce87e88fb9f40b38791
Divya Sathiyamoorthy <sdivya2408@gmail.com>
2022-03-25 14:27:31 -0500

    changed avatar

--- proactivate-web/src/client/Components/Navbar.js
+++ proactivate-web/src/client/Components/Navbar.js
@@ -123,7 +123,7 @@ const ResponsiveAppBar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar src="/broken-image.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
