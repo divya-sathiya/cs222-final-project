35636716f44a39956d5a515476c17ac5ab7b1c46
brendaaurelie <brenda.aurelie@gmail.com>
2022-03-25 16:15:41 -0500

    date picker and progress bar

--- proactivate-web/src/client/Pages/Deadline/Deadline.js
+++ proactivate-web/src/client/Pages/Deadline/Deadline.js
@@ -1,9 +1,13 @@
 import React from "react";
 import CustomizedProgressBars from "../../Components/ProgressBar.js";
+import MaterialUIPickers from "../../Components/DatePicker.js";
+
 const Deadline = () => {
   return( 
   <>
- 
+  <div className="datePick">
+  <MaterialUIPickers/>
+  </div>
   <CustomizedProgressBars />
   </>
    );
