87551f694b8c61bce4c4500c215a385209f1824b
brendaaurelie <brenda.aurelie@gmail.com>
2022-04-08 15:15:35 -0500

    fix table and add trash icon

--- proactivate-web/src/client/Pages/Deadline/Deadline.js
+++ proactivate-web/src/client/Pages/Deadline/Deadline.js
@@ -9,7 +9,26 @@ import DateTimePicker from '@mui/lab/DateTimePicker';
 import CustomizedProgressBars from "../../Components/ProgressBar";
 import Box from '@mui/material/Box';
 import AddIcon from '@mui/icons-material/Add';
+import { makeStyles } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+
 import { IconButton } from "@mui/material";
+
+var tableStyle=
+{
+    "color" :"white",
+    "width": "100%",
+    "table-layout": "fixed"
+};
+
+var headStyle=
+{
+    "margin-top": "3%",
+    "margin-bottom": "3%",
+    "border-bottom":"0.6px solid white"
+};
+
+
 
 const Deadline = () => {
         const [title, setTitle] = useState("");
@@ -21,7 +40,6 @@ const Deadline = () => {
         
         var submitTask = (e) =>
         {
-            e.preventDefault();
             var date = dueDate
             var dd = String(date.getDate()).padStart(2, '0');
             var mm = String(date.getMonth() + 1).padStart(2, '0');
@@ -45,7 +63,7 @@ const Deadline = () => {
         }
 
       
-
+      
         return(
             
         <>
@@ -58,7 +76,7 @@ const Deadline = () => {
         <Box
         component="form"
         sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
+          '& > :not(style)': { m: 1 },
         }}
         noValidate
         autoComplete="off"
@@ -73,22 +91,20 @@ const Deadline = () => {
                 } }/>
         
 
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <LocalizationProvider  color = "primary" dateAdapter={AdapterDateFns}>
                 <DateTimePicker
+                color = "primary"
                 label="Enter Due Date"
                 value={dueDate}
                 onChange={handleChange}
                 renderInput={(params) => <TextField {...params} />}
                 />    
         </LocalizationProvider>
-        <IconButton  onClick={submitTask} color="primary" aria-label="upload picture" component="span">
+
+        <IconButton  onClick={submitTask} sx={{color:"white"}} aria-label="upload picture" size="small">
                 <AddIcon />
             </IconButton></Box>
-        {/* <Button variant="outlined"
-            // onClick={() => {submitTask(); console.log(tasks)}}
-            onClick={submitTask}
-        >Submit</Button>
-    */}
+       
         </Grid>
         <Grid item md>
          
@@ -97,24 +113,28 @@ const Deadline = () => {
     </Box>
        
 
-        <table className="table is-striped is-fullwidth" id="tasksTable">
-                <thead>
+        <table style={tableStyle}>
+                <thead style={headStyle}>
                     <tr>
-                        <td></td>
-                        <th>Assignment Name</th>
-                        <th>Status</th>
+                        <th>Assignment Name</th>          
                         <th>Progress Bar</th>
                         <th>Due Date</th>
+                        <th>Status</th>
                         
                     </tr>
                 </thead>
                 <tbody>
                     { tasks.map((task) => (
                         <tr key={ task.id }>
-                            <td>{ task.title }</td>
-                            <td>{ task.status }</td>
-                            <td><CustomizedProgressBars time={task.time}/></td>
-                            <td>{ task.dueDate }</td>
+                            <td width={"30%"}>{ task.title }</td>
+                            <td width={"50%"}><CustomizedProgressBars time={task.time}/></td>
+                            <td width={"15%"}>{ task.dueDate }</td>
+                            <td width={"5%"}>
+                                <IconButton aria-label="delete" sx={{color:"white"}} size="small">
+                                        <DeleteIcon fontSize="small" />
+                                </IconButton>
+                            </td>
+
                         </tr>
                     )) }
                 </tbody>
